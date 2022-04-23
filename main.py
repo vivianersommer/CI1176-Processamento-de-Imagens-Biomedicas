fc820cfabccfc863be3afc110a45707ff4dd3234
Mists <lmists13@gmail.com>
2022-04-22 22:44:38 -0300

    graficuzinhos

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import glob
 import pydicom as dicom
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
 from tensorflow.python.keras.callbacks import EarlyStopping
 
 from hog import extract_carac
@@ -52,17 +54,27 @@ if __name__ == '__main__':
     print('Qtde de treino: {}'.format(len(x_train)))
     print('Qtde de validação: {}'.format(len(x_val)))
 
-    early_stop = EarlyStopping(monitor='val_loss', patience=30)
+    early_stop = EarlyStopping(monitor='loss', patience=10)
 
     model = Sequential()
-    model.add(Dense(30, input_shape=(16384,), activation='relu'))
+    model.add(Dense(10, input_shape=(16384,), activation='relu'))
+    model.add(Dropout(0.2))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    model.fit(x_train, y_train, epochs=150, batch_size=10, callbacks=early_stop)
+    history = model.fit(x_train, y_train, epochs=150, batch_size=10, callbacks=early_stop)
 
     score = model.evaluate(x_val, y_val, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+
+    fig, ax = plt.subplots(1,2, figsize=(16,8))
+    ax[0].plot(history.history['loss'], color='b', label="Training loss")
+    ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+    legend = ax[0].legend(loc='best', shadow=True)
+
+    ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
+    ax[1].plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+    legend = ax[1].legend(loc='best', shadow=True)
