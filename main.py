b18f8ea2621b40074df29673fda7a5230df91038
Viviane da Rosa Sommer <viviane.sommer@hitechnologies.com.br>
2022-04-22 22:56:06 -0300

    val_loss

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import glob
-import pydicom as dicom
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pydicom as dicom
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.models import Sequential
 from tensorflow.python.keras.callbacks import EarlyStopping
 
 from hog import extract_carac
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from sklearn.model_selection import train_test_split
 
 path_bmt = 'CINTILOGRAFIAS/BMT'
 path_graves = 'CINTILOGRAFIAS/GRAVES'
@@ -39,7 +38,7 @@ if __name__ == '__main__':
         file = file.reshape(128 * 128)
         all.append(file)
         labels.append(0)
-    
+
     for file in graves_images:
         file = file.reshape(128 * 128)
         all.append(file)
@@ -50,7 +49,8 @@ if __name__ == '__main__':
     Y = labels
     Y = np.asarray(Y)
 
-    x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size=0.1, random_state=9)
+    x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size=0.08, random_state=9)
+
     print('Qtde de treino: {}'.format(len(x_train)))
     print('Qtde de validação: {}'.format(len(x_val)))
 
@@ -73,13 +73,13 @@ if __name__ == '__main__':
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-    fig, ax = plt.subplots(1,2, figsize=(16,8))
+    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
     ax[0].plot(history.history['loss'], color='b', label="Training loss")
-    ax[0].plot(history.history['val_loss'], color='r', label="validation loss",axes =ax[0])
+    ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
     legend = ax[0].legend(loc='best', shadow=True)
 
     ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
-    ax[1].plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+    ax[1].plot(history.history['val_accuracy'], color='r', label="Validation accuracy")
     legend = ax[1].legend(loc='best', shadow=True)
 
     plt.show()
