e364d9c99deea743c93f5a1cdf107e13060ce0fe
Mists <lmists13@gmail.com>
2022-04-24 19:45:12 -0300

    progre.\main.pyo

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pydicom as dicom
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LeakyReLU, Dense, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.python.keras.callbacks import EarlyStopping
+from sklearn.model_selection import LeaveOneOut
 
 from hog import extract_carac
 
@@ -48,40 +49,40 @@ if __name__ == '__main__':
     X = np.asarray(X)
     Y = labels
     Y = np.asarray(Y)
+    loo = LeaveOneOut()
+    # x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.08, random_state=9)
+    
+    for train_index, test_index in loo.split(X):
+        
+        x_train, x_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
 
-    x_train, x_val, y_train, y_val = train_test_split(X, Y, test_size=0.08, random_state=9)
+        early_stop = EarlyStopping(monitor='val_loss', patience=10)
 
-    print('Qtde de treino: {}'.format(len(x_train)))
-    print('Qtde de validação: {}'.format(len(x_val)))
+        model = Sequential()
+        model.add(Dense(20, input_shape=(16384,), activation='relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',
+                    optimizer='adam',
+                    metrics=['accuracy'])
 
-    early_stop = EarlyStopping(monitor='val_loss', patience=10)
+        model.fit(x_train, y_train,
+                            epochs=150, batch_size=10,
+                            callbacks=early_stop,
+                            validation_data=(x_test, y_test))
 
-    model = Sequential()
-    model.add(Dense(30, input_shape=(16384,), activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(20, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
 
-    history = model.fit(x_train, y_train,
-                        epochs=150, batch_size=10,
-                        callbacks=early_stop,
-                        validation_data=(x_val, y_val))
+        # fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+        # ax[0].plot(history.history['loss'], color='b', label="Training loss")
+        # ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
+        # legend = ax[0].legend(loc='best', shadow=True)
 
-    score = model.evaluate(x_val, y_val, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
+        # ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
+        # ax[1].plot(history.history['val_accuracy'], color='r', label="Validation accuracy")
+        # legend = ax[1].legend(loc='best', shadow=True)
 
-    fig, ax = plt.subplots(1, 2, figsize=(16, 8))
-    ax[0].plot(history.history['loss'], color='b', label="Training loss")
-    ax[0].plot(history.history['val_loss'], color='r', label="validation loss", axes=ax[0])
-    legend = ax[0].legend(loc='best', shadow=True)
-
-    ax[1].plot(history.history['accuracy'], color='b', label="Training accuracy")
-    ax[1].plot(history.history['val_accuracy'], color='r', label="Validation accuracy")
-    legend = ax[1].legend(loc='best', shadow=True)
-
-    plt.show()
+        # plt.show()
